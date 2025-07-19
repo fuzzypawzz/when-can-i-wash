6673da81a3a8c5125bb78788a9c89b05db8f2fc1
Jannik Maag <55883716+fuzzypawzz@users.noreply.github.com>
2025-07-19 08:58:47 +0200

    Prepare price service

--- src/App.Service/service/user/get-prices.ts
+++ src/App.Service/service/user/get-prices.ts
@@ -1,9 +1,24 @@
 import { httpGateway } from '@/App.Core/infrastructure/http-gateway/http-gateway'
 import type { PriceEntry } from '@/App.Service/models/prices/price-entry'
 
-export async function service_user_getUserDetails(): Promise<PriceEntry[]> {
+/**
+ * Gets the electricity prices as JSON.
+ * Endpoint documentation: https://www.elprisenligenu.dk/elpris-api
+ */
+export async function service_v1_prices(args: {
+  year: number
+  month: number
+  dayOfMonth: number
+  /**
+   * DK1 = Aarhus / Vest for Storebælt
+   * DK2 = København / Øst for Storebælt
+   */
+  priceClass: 'DK1' | 'DK2'
+}): Promise<PriceEntry[]> {
+  /* Example url: https://www.elprisenligenu.dk/api/v1/prices/2025/07-19_DK2.json */
+
   const response = await httpGateway.get(
-    'https://www.elprisenligenu.dk/api/v1/prices/2025/07-15_DK2.json'
+    `https://www.elprisenligenu.dk/api/v1/prices/${args.year}/${args.month}-${args.dayOfMonth}_${args.priceClass}.json`
   )
 
   return response.json()
