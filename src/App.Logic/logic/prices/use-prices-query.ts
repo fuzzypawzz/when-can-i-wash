f62da43bf67a980c9b82abf097fa67d18cb8b07b
Jannik Maag <55883716+fuzzypawzz@users.noreply.github.com>
2025-07-25 08:25:08 +0200

    stop infinite query loops

--- src/App.Logic/logic/prices/use-prices-query.ts
+++ src/App.Logic/logic/prices/use-prices-query.ts
@@ -1,5 +1,5 @@
 import { QueryObserver } from '@tanstack/query-core'
-import { useSyncExternalStore } from 'react'
+import { useCallback, useMemo, useSyncExternalStore } from 'react'
 
 import { queryClient } from '@/App.Core/infrastructure/query-client/query-client'
 import { findCheapestTimeframe } from '@/App.Logic/logic/prices/find-cheapest-timeframe'
@@ -7,26 +7,29 @@ import { priceClassDict } from '@/App.Logic/logic/prices/price-class-dictionary'
 import { service_v1_prices } from '@/App.Service/service/user/get-prices'
 
 export function usePricesQuery() {
-  const observer = new QueryObserver(queryClient.get(), {
-    queryKey: ['prices'],
+  const observer = useMemo(
+    () =>
+      new QueryObserver(queryClient.get(), {
+        queryKey: ['prices'],
 
-    queryFn: () => {
-      return service_v1_prices({
-        year: 2025,
-        month: '07',
-        dayOfMonth: '19',
-        priceClass: priceClassDict['Copenhagen/East']
-      })
-    },
+        queryFn: () => {
+          return service_v1_prices({
+            year: 2025,
+            month: '07',
+            dayOfMonth: '19',
+            priceClass: priceClassDict['Copenhagen/East']
+          })
+        },
 
-    // ! Data should be normalized here
-    select: findCheapestTimeframe,
+        select: (prices) => findCheapestTimeframe(prices),
 
-    throwOnError: true
-  })
-
-  return useSyncExternalStore(
-    (listener) => observer.subscribe(listener),
-    () => observer.getCurrentResult()
+        throwOnError: true
+      }),
+    []
   )
+
+  const subscribe = useCallback((listener: () => void) => observer.subscribe(listener), [observer])
+  const getSnapshot = useCallback(() => observer.getCurrentResult(), [observer])
+
+  return useSyncExternalStore(subscribe, getSnapshot)
 }
